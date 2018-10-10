cb0c363a5807622607546c7862200005277820e3
Bulat <lb6557@gmail.com>
2018-10-10 11:51:30 +0300

    serve by name route

--- src/main/scala/io/hydrosphere/serving/gateway/http/JsonPredictionController.scala
+++ src/main/scala/io/hydrosphere/serving/gateway/http/JsonPredictionController.scala
@@ -1,6 +1,6 @@
 package io.hydrosphere.serving.gateway.http
 
-import akka.http.scaladsl.server.Directives.{as, complete, entity, optionalHeaderValueByName, path, post, _}
+import akka.http.scaladsl.server.Directives._
 import io.hydrosphere.serving.gateway.service.{ApplicationExecutionService, JsonServeByIdRequest, JsonServeByNameRequest, RequestTracingInfo}
 import io.hydrosphere.serving.http.TracingHeaders
 import org.apache.logging.log4j.scala.Logging
@@ -41,38 +41,14 @@ class JsonPredictionController(
   }
 
   def listApps = pathPrefix("applications") {
-    get {
-      complete(gatewayPredictionService.listApps)
-    }
-  }
-
-  def serveById = pathPrefix("applications" / "serveById" / LongNumber / Segment) { (appId, signatureName) =>
-    post {
-      optionalTracingHeaders { (reqId, reqB3Id, reqB3SpanId) =>
-        entity(as[JsObject]) { bytes =>
-          complete {
-            logger.info(s"Serve request: id=$appId signature=$signatureName")
-            gatewayPredictionService.serveJsonById(
-              JsonServeByIdRequest(
-                targetId = appId,
-                signatureName = signatureName,
-                inputs = bytes
-              ),
-              reqId.map(xRequestId =>
-                RequestTracingInfo(
-                  xRequestId = xRequestId,
-                  xB3requestId = reqB3Id,
-                  xB3SpanId = reqB3SpanId
-                )
-              )
-            )
-          }
-        }
+    pathEndOrSingleSlash {
+      get {
+        complete(gatewayPredictionService.listApps)
       }
     }
   }
 
-  def serveByName = pathPrefix("applications" / "serveByName" / Segment / Segment) { (appName, signatureName) =>
+  def serveByName = pathPrefix("applications" / Segment / Segment) { (appName, signatureName) =>
     post {
       optionalTracingHeaders { (reqId, reqB3Id, reqB3SpanId) =>
         entity(as[JsObject]) { jsObject =>
@@ -98,10 +74,10 @@ class JsonPredictionController(
     }
   }
 
-  def v1 = pathPrefix("gateway" / "v1") {
-    listApps ~ serveById ~ serveByName
+  def appRoutes = pathPrefix("gateway") {
+    listApps ~ serveByName
   }
 
-  val routes = compatibleServeById ~ v1
+  val routes = compatibleServeById ~ appRoutes
 
 }
