b91043fcdb5f66a26189132b210fb41b046f0e96
Bulat <lb6557@gmail.com>
2018-10-29 16:58:35 +0300

    Increment grpc dep version and ignore tensor verification for (#7)
    tensorContent cases

---
+++ src/test/scala/io/hydrosphere/serving/gateway/service/ApplicationExecutionServiceSpec.scala
@@ -0,0 +1,35 @@
+package io.hydrosphere.serving.gateway.service
+
+import com.google.protobuf.ByteString
+import io.hydrosphere.serving.gateway.GenericTest
+import io.hydrosphere.serving.tensorflow.TensorShape
+import io.hydrosphere.serving.tensorflow.api.predict.PredictRequest
+import io.hydrosphere.serving.tensorflow.tensor.{TensorProto, Uint8Tensor}
+import io.hydrosphere.serving.tensorflow.types.DataType.DT_INT16
+
+class ApplicationExecutionServiceSpec extends GenericTest {
+
+  describe("ApplicationExecutionService") {
+    it("should skip verifications for tensors with non-empty tensorContent field") {
+      val applicationExecutionService = new ApplicationExecutionServiceImpl(
+        null, null, null, null, null
+      )
+      val ignoreMe = TensorProto(
+        dtype = DT_INT16,
+        tensorContent = ByteString.copyFromUtf8("testtest")
+      )
+      val noticeMe = Uint8Tensor(TensorShape.scalar, Seq(1, 2, 3)).toProto
+      val request = PredictRequest(
+        inputs = Map(
+          "ignoreMe" -> ignoreMe,
+          "noticeMe" -> noticeMe
+        )
+      )
+      val result = applicationExecutionService.verify(request)
+      assert(result.isRight)
+      val r = result.right.get
+      assert(r.inputs("ignoreMe") === ignoreMe)
+      assert(r.inputs("noticeMe") === noticeMe)
+    }
+  }
+}
