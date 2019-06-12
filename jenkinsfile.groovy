c3cc61fc241d1b63ef9db63cab1569e54809a274
Vadim Chelyshov <qtankle@gmail.com>
2019-06-12 23:50:19 +0300

    wip

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,11 +3,11 @@ node {
   stage("trigger-central") {
     build job: 'provectus.com/hydro-central/master', parameters: [
       [$class: 'StringParameterValue',
-      name: 'repo',
+      name: 'PROJECT',
       value: 'gateway'
       ],
       [$class: 'StringParameterValue',
-      name: 'branch',
+      name: 'BRANCH',
       value: env.BRANCH_NAME
       ]
     ]
