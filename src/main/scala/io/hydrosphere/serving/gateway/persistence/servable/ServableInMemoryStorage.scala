98ababc57538d7dc679bb686d2beb353c4b21bda
Bulat <lb6557@gmail.com>
2019-06-13 12:16:58 +0300

    remove servable counter from state

--- src/main/scala/io/hydrosphere/serving/gateway/persistence/servable/ServableInMemoryStorage.scala
+++ src/main/scala/io/hydrosphere/serving/gateway/persistence/servable/ServableInMemoryStorage.scala
@@ -12,8 +12,6 @@ import io.hydrosphere.serving.gateway.util.ReadWriteLock
 import scala.collection.mutable
 
 /**
-  * We don't have Application and Servable statuses implemented yet.
-  * Thus, we implemented a hack that track number of servable allocations to try to track actual infrastructure.
   * @param lock read write lock implementation to synchronize concurrent access to the inner state
   * @tparam F Effectful type
   */
@@ -26,7 +24,6 @@ class ServableInMemoryStorage[F[_]](
   clock: Clock[F]
 ) extends ServableStorage[F] {
   private[this] val servableState = mutable.Map.empty[String, StoredServable]
-  private[this] val servableCounter = mutable.Map.empty[String, Long]
   private[this] val servableExecutors = mutable.Map.empty[String, CloseablePredictor[F]]
   private[this] val monitorableExecutors = mutable.Map.empty[String, Predictor[F]]
 
@@ -45,9 +42,6 @@ class ServableInMemoryStorage[F[_]](
         servableState.get(s.name) match {
           case Some(_) =>
             for {
-              oldCounter <- F.delay(servableCounter.getOrElseUpdate(s.name, 0))
-              _ <- F.delay(servableCounter.update(s.name, oldCounter + 1))
-
               _ <- F.delay(servableState.update(s.name, s))
 
               _ <- OptionT(F.delay(servableExecutors.get(s.name)))
@@ -64,7 +58,6 @@ class ServableInMemoryStorage[F[_]](
               exec <- Predictor.forServable(s, clientCtor)
               shadowed = Predictor.withShadow(s, exec, shadow, None)
               _ <- F.delay(servableState += s.name -> s)
-              _ <- F.delay(servableCounter += s.name -> 1)
               _ <- F.delay(servableExecutors += s.name -> exec)
               _ <- F.delay(monitorableExecutors += s.name -> shadowed)
             } yield ()
@@ -76,22 +69,12 @@ class ServableInMemoryStorage[F[_]](
   override def remove(ids: Seq[String]): F[Unit] = {
     lock.write.use { _ =>
       ids.toList.traverse { name =>
-        servableCounter.get(name) match {
-          case Some(counter) =>
-            counter match {
-              case 1 =>
-                for {
-                  _ <- F.delay(servableState -= name)
-                  _ <- F.delay(servableCounter -= name)
-                  _ <- servableExecutors(name).close
-                  _ <- F.delay(servableExecutors -= name)
-                  _ <- F.delay(monitorableExecutors -= name)
-                } yield ()
-              case x =>
-                F.delay(servableCounter.update(name, x - 1))
-            }
-          case None => F.unit
-        }
+        for {
+          _ <- F.delay(servableState -= name)
+          _ <- servableExecutors(name).close
+          _ <- F.delay(servableExecutors -= name)
+          _ <- F.delay(monitorableExecutors -= name)
+        } yield ()
       }.void
     }
   }
