ce2f01de40fd9bb10917c3c051693c9546826218
Konstantin Makarychev <mrsimpson@inbox.ru>
2019-07-19 12:00:14 +0300

    Switch to grpc for reqstore requests

--- src/main/scala/io/hydrosphere/serving/gateway/integrations/reqstore/ReqStore.scala
+++ src/main/scala/io/hydrosphere/serving/gateway/integrations/reqstore/ReqStore.scala
@@ -1,13 +1,25 @@
 package io.hydrosphere.serving.gateway.integrations.reqstore
 
+import java.time.Instant
+
+import akka.actor.ActorSystem
 import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.util.ByteString
 import cats.effect.{Async, Sync}
 import cats.syntax.either._
 import cats.syntax.flatMap._
 import cats.syntax.functor._
+import io.grpc.{ClientInterceptors, ManagedChannelBuilder}
 import io.hydrosphere.serving.gateway.config.ReqStoreConfig
+import io.hydrosphere.serving.grpc.{AuthorityReplacerInterceptor, Headers}
+import io.hydrosphere.reqstore.reqstore_service.{SaveRequest, TimemachineGrpc}
+import io.hydrosphere.serving.gateway.util.AsyncUtil
 import io.hydrosphere.serving.monitoring.metadata.TraceData
 import org.apache.logging.log4j.scala.Logging
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 trait ReqStore[F[_], A] {
   def save(name: String, a: A): F[TraceData]
@@ -18,6 +30,30 @@ object ReqStore extends Logging {
   import jsonCodecs._
   import spray.json._
 
+  def grpc[F[_], A](cfg: ReqStoreConfig)(implicit F: Async[F], tbs: ToByteSource[A]): F[ReqStore[F, A]] = F.delay {
+    new ReqStore[F, A] {
+
+      implicit val system = ActorSystem("QuickStart")
+      implicit val materializer = ActorMaterializer()
+
+      private lazy val grpcChannel = {
+        val deadline = 2 minutes
+        val builder = ManagedChannelBuilder.forAddress(cfg.host, cfg.port)
+        builder.enableRetry()
+        builder.usePlaintext()
+        builder.keepAliveTimeout(deadline.length, deadline.unit)
+        ClientInterceptors.intercept(builder.build(), new AuthorityReplacerInterceptor +: Headers.interceptors: _*)
+      }
+
+      private lazy val reqstoreChannel = TimemachineGrpc.stub(grpcChannel)
+
+      override def save(name: String, a: A): F[TraceData] = for {
+        data <- AsyncUtil.futureAsync(tbs.asByteSource(a).source.runFold(ByteString.empty)(_ ++ _))
+        id <- AsyncUtil.futureAsync(reqstoreChannel.save(SaveRequest(name, false, com.google.protobuf.ByteString.copyFrom(data.asByteBuffer), Instant.now().toEpochMilli)))
+      } yield TraceData(id.timestamp, id.unique)
+    }
+  }
+  
   def create[F[_], A](cfg: ReqStoreConfig)(
     implicit
     F: Async[F],
