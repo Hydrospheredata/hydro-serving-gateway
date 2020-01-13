261e91f5d6de7cc081c95738d18705cb009126ab
Dmitriy Kharlamov <dkharlamov@provectus.com>
2020-01-13 14:36:44 +0300

    add sonar stage

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,14 @@
 node {
+  stage('SCM') {
+    git 'https://github.com/Hydrospheredata/hydro-serving-gateway.git'
+  }
+  stage('SonarQube analysis') {
+    def scannerHome = tool 'SonarScanner 4.0';
+    withSonarQubeEnv('Sonarcloud') { // If you have configured more than one global server connection, you can specify its name
+      sh "${scannerHome}/bin/sonar-scanner"
+    }
+  }
+
   stage("trigger-central") {
     build job: 'provectus.com/hydro-central/master', parameters: [
       [$class: 'StringParameterValue',
