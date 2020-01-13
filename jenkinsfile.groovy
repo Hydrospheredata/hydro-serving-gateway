f555ba8ef8d33c91a98e545a6244425d33b4b596
Dmitriy Kharlamov <dkharlamov@provectus.com>
2020-01-13 15:15:25 +0300

    add sonar branch support

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,10 +9,23 @@ node {
          -Dsonar.projectKey=Hydrospheredata_hydro-serving-gateway \
          -Dsonar.organization=hydrosphere \
          -Dsonar.sources=. \
+         -Dsonar.branch.name=${GIT_BRANCH} \
+         -Dsonar.pullrequest.key=${CHANGE_ID} \
+         -Dsonar.pullrequest.branch=${CHANGE_BRANCH} \
+         -Dsonar.pullrequest.base=${CHANGE_TARGET} \
          -Dsonar.host.url=https://sonarcloud.io \
          -Dsonar.login=f4edb54bde6f29b48660b944fda885099b9a2a48"
     }
   }
+
+//  stage("Quality Gate"){
+//    timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+//      def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+//      if (qg.status != 'OK') {
+//        error "Pipeline aborted due to quality gate failure: ${qg.status}"
+//      }
+//    }
+//  }
 
   stage("trigger-central") {
     build job: 'provectus.com/hydro-central/master', parameters: [
