41730cb7d34976bbaa63b76015f59b287c609a07
Paul Mck <vixtir90@gmail.com>
2021-06-18 10:26:36 +0300

    chore: bump scala to 2.13.2 with dependencies

--- src/main/scala/io/hydrosphere/serving/gateway/config/Configuration.scala
+++ src/main/scala/io/hydrosphere/serving/gateway/config/Configuration.scala
@@ -1,17 +1,15 @@
 package io.hydrosphere.serving.gateway.config
 
 import cats.effect.Sync
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
 
 final case class Configuration(application: ApplicationConfig)
 
 object Configuration  {
   final case class ConfigurationLoadingException(msg: String) extends Throwable
 
-  def load[F[_]](implicit F: Sync[F]) = F.defer {
-    F.fromEither {
-      pureconfig.loadConfig[Configuration].left.map { x =>
-        ConfigurationLoadingException(x.toList.mkString("\n"))
-      }
-    }
+  def load[F[_]](implicit F: Sync[F]):F[Configuration] = F.delay {
+      ConfigSource.default.loadOrThrow[Configuration]
   }
 }
